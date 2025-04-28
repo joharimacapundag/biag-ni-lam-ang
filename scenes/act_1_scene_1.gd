92ac3c3e00b1bb504179a9a85e3a307e0b086166
Gragnauty <vhincecedrick.afroilan@lorma.edu>
2025-04-28 23:31:50 +0800

    act 2 & 3

--- scenes/act_1_scene_1.gd
+++ scenes/act_1_scene_1.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 func _ready() -> void:
-	Dialogic.start("act_2_ines")
+	Dialogic.start("act_2_dog_scene")
 	await Dialogic.timeline_ended
 	GameEvents.travel_started.emit()
 	await get_tree().create_timer(7).timeout
 	%AudioStreamPlayer2D.stop()
-	get_tree().change_scene_to_file("uid://fjdph61htgra")
+	get_tree().change_scene_to_file("uid://buc2v2ocwo4ci")
 	

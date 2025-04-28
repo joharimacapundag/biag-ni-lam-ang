191ceeaa62a1feed22232d9a33fb8d155b2012ee
Gragnauty <vhincecedrick.afroilan@lorma.edu>
2025-04-29 01:12:33 +0800

    scenes

--- scenes/act2&3/act_3_scene_1.gd
+++ scenes/act2&3/act_3_scene_1.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 func _ready() -> void:
-	Dialogic.start("act_1_scene_1_a")
+	Dialogic.start("village_stop")
 	await Dialogic.timeline_ended
 	GameEvents.travel_started.emit()
 	await get_tree().create_timer(7).timeout
 	%AudioStreamPlayer2D.stop()
-	get_tree().change_scene_to_file("uid://fjdph61htgra")
+	get_tree().change_scene_to_file("uid://jbxdbbvx51tl")
 	
